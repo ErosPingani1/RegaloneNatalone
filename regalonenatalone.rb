c67975adbbeb03e5219ac59303b471b8fa5ddf19
Eros Pingani <e.pingani3@gmail.com>
2020-12-04 08:31:49 +0100

    [DEV][BE] - Try again managed when someone picks itself

--- regalonenatalone.rb
+++ regalonenatalone.rb
@@ -3,49 +3,33 @@ require 'json'
 
 class RegaloneNatalone < Sinatra::Base
     get '/regalonenatalone' do
-        randomPresentSelector()
-    end
-    get '/riprovaesaraipiufortunato' do
-        tryAgain()
+        eljugador = params['eljugador']
+        randomPresentSelector(eljugador)
     end
 end
 
-def randomPresentSelector()
+def randomPresentSelector(eljugador)
     #Lettura JSON e scrittura variabili persone disponibili o selezionate
     file = File.read('./loschiindividui.json')
     loschiindividui = JSON.parse(file)['loschiindividui']
     chosenones = JSON.parse(file)['chosenones']
-
     #Estrazione random di chi riceverà il regalo tra le persone disponibili
     ilfortunato = loschiindividui[rand(loschiindividui.length())]
-    loschiindividui.delete(ilfortunato)
-    chosenones.push(ilfortunato)
-
-    #Riscrittura file JSON
-    updateJSONFile(file, loschiindividui, chosenones)
-
-    #Risposta persona selezionata
-    content_type :json
-    ilfortunato.to_json
+    if (ilfortunato['nome'].downcase == eljugador.downcase)
+        tryAgain(eljugador)
+    else
+        loschiindividui.delete(ilfortunato)
+        chosenones.push(ilfortunato)
+        #Riscrittura file JSON
+        updateJSONFile(file, loschiindividui, chosenones)
+        #Risposta persona selezionata
+        content_type :json
+        ilfortunato.to_json
+    end
 end
 
-def tryAgain()
-    #Lettura JSON e scrittura variabili persone disponibili o selezionate
-    file = File.read('./loschiindividui.json')
-    loschiindividui = JSON.parse(file)['loschiindividui']
-    chosenones = JSON.parse(file)['chosenones']
-
-    #Selezione ultima persona ottenuta e cancellazione dalla lista selezionate
-    notyetchosen = chosenones.last
-    puts('chosenones before edit: ', chosenones)
-    chosenones.delete(notyetchosen)
-    puts('chosenones after edit: ', chosenones)
-    loschiindividui.push(notyetchosen)
-
-    #Update del file JSON e riavvio della selezione random
-    updateJSONFile(file, loschiindividui, chosenones)
-    randomPresentSelector()
-
+def tryAgain(eljugador)
+    randomPresentSelector(eljugador)
 end
 
 def updateJSONFile(file, loschiindividui, chosenones)
